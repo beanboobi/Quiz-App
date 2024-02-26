14da21d220245d9953e1474fbe97a4066114d945
minli3110 <127458261+minli3110@users.noreply.github.com>
2024-02-26 21:44:04 +0800

    Add files via upload

---
+++ QuizConnect/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using QuizConnect.Pages;
+
+namespace QuizConnect
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+    }
+}
