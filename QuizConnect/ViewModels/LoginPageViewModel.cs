0b79f2e8dbb023025db6dee56d9b7c13d646ca03
minli3110 <127458261+minli3110@users.noreply.github.com>
2024-02-28 21:25:10 +0800

    Add files via upload

---
+++ QuizConnect/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace QuizConnect.ViewModels
+{
+    internal class LoginPageViewModel : BaseViewModel
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set => SetProperty(ref _username, value);
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public IRelayCommand LoginCommand { get; }
+
+        public LoginPageViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+        }
+
+        private async void Login()
+        {
+            // Implement login logic here
+        }
+    }
+}
